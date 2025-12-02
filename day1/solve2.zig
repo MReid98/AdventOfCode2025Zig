9a5a4468d8f7f7683adbba08ce1628e3caa4115b
Mark Reid <mreid98@gmail.com>
2025-12-02 20:08:47 +0000

    Finish day 1

--- day1/solve2.zig
+++ day1/solve2.zig
@@ -14,21 +14,20 @@ pub fn main() !void {
     var reader = file.reader(&file_buffer);
 
     var pos: i32 = 50;
-    var zeros: u32 = 0;
     var lines: u32 = 0;
+    var totalPasses: u32 = 0;
 
     while (try reader.interface.takeDelimiter('\n')) |line| {
+        var passes: u32 = 0;
         lines += 1;
-        pos = try turn(pos, line);
-        if (pos == 0) {
-            zeros += 1;
-        }
+        pos, passes = try turn(pos, line);
+        totalPasses += passes;
     }
 
-    print("Total zeros: {d}\n", .{zeros});
+    print("Total passes: {d}\n", .{totalPasses});
 }
 
-fn turn(pos: i32, input: []u8) !i32 {
+fn turn(pos: i32, input: []u8) !struct{i32, u32} {
     const lr: u8 = input[0];
 
     const sign: i32 = switch(lr) {
@@ -37,8 +36,16 @@ fn turn(pos: i32, input: []u8) !i32 {
         else => 0,
     };
 
+    const rotation = pos + (try parseInt(i32, input[1..], 0) * sign);
+
+    var passes: u32 = @abs(@divTrunc(rotation, 100));
+    if (rotation <= 0 and pos > 0) {
+        passes += 1;
+    }
+
     try std.testing.expect(sign != 0);
 
-    const newPos = @mod((try parseInt(i32, input[1..], 0) * sign) + pos, 100);
-    return newPos;
+    const newPos = @mod(rotation, 100);
+
+    return .{newPos, @abs(passes)};
 }
